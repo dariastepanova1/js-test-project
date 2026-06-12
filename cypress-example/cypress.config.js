f2eac36924bdc1a6d07e789bd2f29fe4bc5d1725
Daria Stepanova <testdasha1@gmail.com>
2026-06-12 12:20:10 +0300

    add hw 18

---
+++ cypress-example/cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,  
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  video: true,
+  viewportHeight: 720,
+  viewportWidth: 1080,
+
+e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+       on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+      })
+    },
+    baseUrl: 'https://example.cypress.io',
+    specPattern: 'cypress/e2e/**/*.test.js',
+  },
+});
