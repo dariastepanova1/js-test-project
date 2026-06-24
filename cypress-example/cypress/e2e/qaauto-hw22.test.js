7756b984f48c8ca15b1e3f24ddf236f7ae4a4f92
Daria Stepanova <testdasha1@gmail.com>
2026-06-24 17:50:08 +0300

    update expenses test

--- cypress-example/cypress/e2e/qaauto-hw22.test.js
+++ cypress-example/cypress/e2e/qaauto-hw22.test.js
@@ -57,19 +57,28 @@ describe("Add Car and Fuel expenses mixed UI and API tests", () => {
 
     cy.intercept("POST", "/api/cars").as("createCar");
     addCarDialog.addCar(carBrand, carModel, expenseMileage);
+
     cy.wait("@createCar").then(({ response }) => {
       expect(response.statusCode).to.eq(201);
       cy.wrap(response.body.data.id).as("carId");
     });
 
     cy.get("@carId").then((carId) => {
-      cy.createExpense(carId, newExpenseMileage, liters, expenses);
-    });
+      cy.createExpense(carId, newExpenseMileage, liters, expenses).then(
+        (response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.exist;
+          expect(response.body.data.mileage).to.eq(newExpenseMileage);
+          expect(response.body.data.liters).to.eq(liters);
+          expect(response.body.data.totalCost).to.eq(expenses);
+        },
+      );
 
-    leftNavBar.clickNavItem("Fuel expenses");
-    fuelExpensesPage.selectCar(carBrand, carModel);
-    fuelExpensesPage.checkMileage(newExpenseMileage);
-    fuelExpensesPage.checkLiters(liters),
-    fuelExpensesPage.checkCost(expenses);
+      leftNavBar.clickNavItem("Fuel expenses");
+      fuelExpensesPage.selectCar(carBrand, carModel);
+      fuelExpensesPage.checkMileage(newExpenseMileage);
+      fuelExpensesPage.checkLiters(liters);
+      fuelExpensesPage.checkCost(expenses);
+    });
   });
 });
